ae23e796103892ca3eeb91b647db0a4c56e82d4c
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-05-08 14:45:54 +0900

    ui: 상세페이지 완성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import user from '@/assets/icons/user.svg';
 
 export default function Header() {
   return (
-    <header className="fixed flex h-20 w-full items-center justify-between border-b border-(--color-line-100) bg-white px-30 py-6.5">
+    <header className="top-0 left-0 z-50 fixed flex h-20 w-full items-center justify-between border-b border-(--color-line-100) bg-white px-30 py-6.5">
       <div className="flex items-center gap-9">
         <img src={logo} alt="Epigram"></img>
         <div className="text-base font-semibold text-center justify-start text-neutral-700 leading-6">
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,72 +1,124 @@
 import Header from '@/components/Header/Header';
+import waveLine from '@/assets/images/waveLine.svg';
+import more from '@/assets/icons/more-md.png';
+import share from '@/assets/icons/Share.png';
+import like from '@/assets/icons/like.png';
+import profileImage from '@/assets/profileImages/profile01.png';
+
+const mockEpigrams = {
+    id: 1,
+    content: '오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다.',
+    tags: ['나아가야 할때', '꿈을 이루고 싶을 때'],
+    author: '안나',
+    likeCount: 21,
+  }
+
+const mockComments = [
+  {
+    id: 1,
+    image: '/src/assets/profileImages/profile01.png',
+    userName: '지킬과 하이드',
+    time: '1시간 전',
+    content: '오늘 하루 우울했는데 덕분에 많은 힘 얻고 갑니다.',
+    isMine: true, 
+  },
+
+  {
+    id: 2,
+    image: '/src/assets/profileImages/profile01.png',
+    userName: '노인과 바다',
+    time: '2시간 전',
+    content: '별로네요...',
+    isMine: false, 
+  },
+
+  {
+    id: 3,
+    image: '/src/assets/profileImages/profile01.png',
+    userName: '빨간머리 앤',
+    time: '2시간 전',
+    content: '재밌겠다...',
+    isMine: false, 
+  }
+]
 
 export default function EpigramDetailPage() {
+  
+  
   return (
     <>
       <Header />
 
       <main className="min-h-screen bg-[#F5F7FA]">
+
         {/* 에피그램 상세 영역 */}
-        <section className="bg-white">
-          <div className="mx-auto w-[640px] pt-[36px] pb-[32px]">
+        <section className="bg-white relative h-[472px] bg-position-[center_top]">
+          <div className="mx-auto w-[640px] pt-[120px] pb-[32px]">
+
+
             {/* 태그 + 더보기 버튼 */}
             <div className="mb-[24px] flex items-center justify-between">
-              <div className="flex gap-[12px] text-[14px] text-[#ABB8CE]">
-                <span>#꿈을이루고싶을때</span>
-                <span>#나아가야할때</span>
+
+              {/*태그*/}
+              <div className="font-['Pretendard'] flex gap-[12px] text-[20px] text-(--color-blue-400)">
+                {mockEpigrams.tags.map((tag)=> (
+                  <span key={tag}>#{tag}</span>
+                ))}
               </div>
 
-              <button
-                type="button"
-                className="text-[24px] leading-none text-[#ABB8CE]"
-              >
-                ⋮
+              {/*더보기 버튼*/}
+              <button type="button">
+                <img src={more} className="w-[36px]"/>
               </button>
             </div>
 
+
             {/* 본문 */}
-            <p className="font-['Iropke_Batang_OTF'] text-[24px] leading-[40px] text-[#2B2B2B]">
-              오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아간다.
+            <p className="font-['Iropke_Batang_OTF'] text-[32px] leading-[40px] text-(--color-black-700)">
+              {mockEpigrams.content}
             </p>
+
 
             {/* 작가 */}
-            <p className="mt-[32px] text-center text-[18px] text-[#ABB8CE]">
-              - 앙드레 말로 -
+            <p className="mt-[32px] font-['Iropke_Batang_OTF'] text-right text-[24px] text-[#ABB8CE]">
+              - {mockEpigrams.author} -
             </p>
 
+
             {/* 좋아요 / 공유 버튼 */}
-            <div className="mt-[32px] flex justify-center gap-[12px]">
+            <div className="mt-[36px] flex justify-center gap-[12px]">
               <button
                 type="button"
-                className="flex h-[36px] items-center gap-[6px] rounded-full bg-[#2B2B2B] px-[16px] text-[14px] font-semibold text-white"
+                className="flex h-[48px] items-center gap-[6px] rounded-full bg-(--color-black-600) px-[16px] text-[20px] font-['Pretendard'] font-semibold text-white"
               >
-                👍
-                <span>123</span>
+                <img src={like} className="w-[36px] h-[36px]" />
+                <span>{mockEpigrams.likeCount}</span>
               </button>
 
               <button
                 type="button"
-                className="h-[36px] rounded-full bg-[#F2F4F7] px-[16px] text-[14px] font-semibold text-[#8B95A1]"
+                className="flex h-[48px] items-center gap-[6px] rounded-full bg-(--color-line-100) px-[16px] text-[20px] font-['Pretendard'] font-medium text-(--color-gray-300)"
               >
-                왕도로 가는 길 ↗
+                <span>왕도로 가는 길</span>
+                <img src={share} className="w-[21px] h-[21px]"/>
               </button>
             </div>
           </div>
 
-          {/* 흰 영역 아래 지그재그 느낌 */}
-          <div className="h-[12px] bg-[repeating-linear-gradient(135deg,#ffffff_0px,#ffffff_8px,#F5F7FA_8px,#F5F7FA_16px)]" />
+          
+          <img src={waveLine} alt="" className="absolute bottom-[-24px] w-full"/>
         </section>
 
         {/* 댓글 영역 */}
         <section className="mx-auto w-[640px] pt-[36px]">
           <h2 className="mb-[16px] text-[16px] font-semibold text-[#2B2B2B]">
-            댓글 <span>(3)</span>
+            댓글 <span>({mockComments.length})</span>
           </h2>
 
           {/* 댓글 입력 */}
           <div className="mb-[28px] flex gap-[16px]">
             <img
-              src="/src/assets/images/profile.png"
+              src={profileImage}
               alt="profile"
               className="h-[40px] w-[40px] rounded-full object-cover"
             />
@@ -78,28 +130,18 @@ export default function EpigramDetailPage() {
             />
           </div>
 
+          {/*이미 달린 댓글*/}
           <div className="border-t border-[#D7E0EE]">
-            <CommentItem
-              image="/src/assets/images/profile.png"
-              name="지킬과 하이드"
-              time="1시간 전"
-              content="오늘 하루 우울했는데 덕분에 많은 힘 얻고 갑니다. 연금술사 책 다시 사서 오랜만에 읽어봐야겠어요!"
-              isMine
-            />
-
-            <CommentItem
-              image="/src/assets/images/profile.png"
-              name="지킬과 하이드"
-              time="1시간 전"
-              content="오늘 하루 우울했는데 덕분에 많은 힘 얻고 갑니다. 연금술사 책 다시 사서 오랜만에 읽어봐야겠어요!"
-            />
-
-            <CommentItem
-              image="/src/assets/images/profile.png"
-              name="지킬과 하이드"
-              time="1시간 전"
-              content="오늘 하루 우울했는데 덕분에 많은 힘 얻고 갑니다. 연금술사 책 다시 사서 오랜만에 읽어봐야겠어요!"
-            />
+            {mockComments.map((comment)=>(
+              <CommentItem
+                key={comment.id}
+                image={comment.image}
+                name={comment.userName}
+                time={comment.time}
+                content={comment.content}
+                isMine={comment.isMine}
+              />
+            ))}
           </div>
         </section>
       </main>
@@ -154,4 +196,3 @@ function CommentItem({
     </article>
   );
 }
-
